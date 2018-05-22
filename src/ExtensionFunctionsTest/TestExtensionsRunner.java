786607aa596cb702f9caf05b5e6fee40b2332b1b
Jonas Svensson <wulfhaven@gmail.com>
2018-05-22 19:18:52 +0200

    Using extension function from java the correct way! :D

--- src/ExtensionFunctionsTest/TestExtensionsRunner.java
+++ src/ExtensionFunctionsTest/TestExtensionsRunner.java
@@ -4,5 +4,6 @@ public class TestExtensionsRunner {
     public static void main(String... args) {
         TestExtensionsPOJO obj = new TestExtensionsPOJO();
         //System.out.println(obj.add()); <-- dNO WORKYWORKY
+        System.out.println(TestExtensionsFunctionsKt.add(obj));
     }
 }
